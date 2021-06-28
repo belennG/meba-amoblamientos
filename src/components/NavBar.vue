f1239b6f4c0735f4668804138149fd8f33de01ad
Maria Belen Gatti <mbelen@hotmail.com>
2021-06-28 20:54:53 +0000

    Added navbar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <h1>It works</h1>
+    <div class="navbar flex">
+      <img src="@/assets/MEBAlogo.jpeg" alt="" class="h-24 w-24" />
+      <h1 class="">Amoblamientos MEBA</h1>
+      <div class="links flex items-end">
+        <a href="#">Inicio</a>
+        <a href="#">Quiénes Somos</a>
+        <a href="#">Nuestros Proyectos</a>
+        <a href="#">Contacto</a>
+      </div>
+    </div>
   </div>
 </template>
 
