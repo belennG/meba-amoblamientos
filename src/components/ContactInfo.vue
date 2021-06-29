5938ac789c2b0cc7e20b046033aeec25b031061a
belennG <mbelen.gatti@hotmail.com>
2021-06-29 23:26:32 +0200

    Add link and icons to contactinfo

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    <div>
-      <i class="fas fa-phone-alt"></i>
-      <p>+54 2346 408639</p>
-    </div>
+    <span>
+      <p><i class="fas fa-phone-alt pr-2"></i>+54 2346 408639</p>
+    </span>
+    <span class="flex justify-end">
+      <a href="amoblamientosmeba@hotmail.com" target="_blank" class="pr-2 pl-2"
+        ><i class="far fa-envelope"></i
+      ></a>
+      <a
+        href="https://www.facebook.com/amoblamientosmeba"
+        target="_blank"
+        class="pr-2 pl-2"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
+      <a
+        href="https://www.instagram.com/meba.amoblamientos"
+        target="_blank"
+        class="pr-2 pl-2"
+        ><i class="fab fa-instagram"></i
+      ></a>
+    </span>
   </div>
 </template>
 
